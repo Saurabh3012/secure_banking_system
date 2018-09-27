204b39e4d259b84431519bb178c4d44c1bb34f44
Dwaraka01 <vallamchetty17067@iiitd.ac.in>
2018-09-27 18:30:17 +0530

    upated few files

--- routes/trasaction.js
+++ routes/trasaction.js
@@ -1,7 +1,23 @@
 // TODO: this file gets triggered with a transaction is initiated.
+var express = require('express');
+var router = express.Router();
 
-// take amount, finish teh transaction.
+var users_transactions = require('../models/users_transaction');
 
-// input : nothing (amount maybe)
+router.post('/external_transaction', function (req, res, next) {
+    var user_id = req.body.user_id;
 
-// output: status, entry in corresponding table 
+    // TODO: create a new table for transactions
+
+    // TODO: query for the transactions, and pass user_id to this page
+
+    // TODO: Crate a tab;e for account numbers.
+    var sender_acc = req.body.sender_acc_num;
+    var receiver_acc = req.body.receiver_acc_num;
+    var amount = req.body.amount;
+    var remarks = req.body.remarks;
+    var status = "Pending";
+    users_transactions.insertOne(user_id, sender_acc, receiver_acc, amount, remarks, status);
+});
+
+module.exports = router;
