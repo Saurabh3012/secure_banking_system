6ab217d60103919f4e003fc0cdc04f3de7d1e3ca
Avinash Tulasi <avinasht@iiitd.ac.in>
2018-10-23 14:40:35 +0530

    added pages for users 3 and 4. updated banking.js to fetch details exclusively for summary.

--- routes/banking.js
+++ routes/banking.js
@@ -41,18 +41,27 @@ router.get("/", function (req, res) {
                 }
                 else if (req.user.role == 2) {
                     res.render("account_summary_2", {
-                        title:"Regular Employee Dashboard"
+                        title:"Regular Employee Dashboard",
+                        trans: trans
                     })
                 }
                 else if (req.user.role == 3) {
-                    res.render("account_summamry_3", {
-                        title: "External User Dashboard."
+                    res.render("account_summary_3", {
+                        title: "External User Dashboard.",
+                        trans: trans
+                    })
+                }
+                else if (req.user.role == 4) {
+                    res.render("account_summary_4", {
+                        title: "External User Dashboard.",
+                        trans: trans
                     })
                 }
                 else {
                     // TODO: merchant payee (user role 4)
                     res.render("make_transaction", {
-                        title: "Make a transaction"
+                        title: "Make a transaction",
+                        trans: trans
                     })
                 }
             }
